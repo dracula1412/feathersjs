ccb435d1fe71b456f1a890c2d329b201fa54bc67
hungkm <hungkm@fsoft.com.vn>
2018-03-16 15:45:56 +0700

    The Basics > Clients

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const memory = require('feathers-memory');
 
 const app = express(feathers());
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Turn on JSON body parsing for REST services
 app.use(express.json())
 // Turn on URL-encoded body parsing for REST services
@@ -41,49 +48,3 @@ app.service('messages').create({
 });
 
 server.on('listening', () => console.log('Feathers REST API started at localhost:3030'));
-
-async function createAndFind() {
-  // Stores a reference to the messages service so we don't have to call it all the time
-  const messages = app.service('messages');
-
-  for(let counter = 0; counter < 100; counter++) {
-    await messages.create({
-      counter,
-      message: `Message number ${counter}`
-    });
-  }
-
-  // We show 10 entries by default. By skipping 10 we go to page 2
-  const page2 = await messages.find({
-    query: { $skip: 10 }
-  });
-
-  console.log('Page number 2', page2);
-
-  // Show 20 items per page
-  const largePage = await messages.find({
-    query: { $limit: 20 }
-  });
-
-  console.log('20 items', largePage);
-
-  // Find the first 10 items with counter greater 50 and less than 70
-  const counterList = await messages.find({
-    query: {
-      counter: { $gt: 50, $lt: 70 }
-    }
-  });
-
-  console.log('Counter greater 50 and less than 70', counterList);
-
-  // Find all entries with text "Message number 20"
-  const message20 = await messages.find({
-    query: {
-      message: 'Message number 20'
-    }
-  });
-
-  console.log('Entries with text "Message number 20"', message20);
-}
-
-createAndFind();
--- public/client.js
+++ public/client.js
@@ -1,19 +1,22 @@
-/* global io */
-
 // Create a websocket connecting to our Feathers server
 const socket = io('http://localhost:3030');
+// Create a Feathers application
+const app = feathers();
+// Configure Socket.io client services to use that socket
+app.configure(feathers.socketio(socket));
 
-// Listen to new messages being created
-socket.on('messages created', message =>
-  console.log('Someone created a message', message)
-);
-
-socket.emit('create', 'messages', {
-  text: 'Hello from socket'
-}, (error, result) => {
-  if (error) throw error
-  socket.emit('find', 'messages', (error, messageList) => {
-    if (error) throw error
-    console.log('Current messages', messageList);
-  });
+app.service('messages').on('created', message => {
+  console.log('Someone created a message', message);
 });
+
+async function createAndList() {
+  await app.service('messages').create({
+    text: 'Hello from Feathers browser client'
+  });
+
+  const messages = await app.services('messages').find();
+
+  console.log('Messages', messages);
+}
+
+createAndList();
